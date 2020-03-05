04137725cd4336c1c2bf46a290ac205c09db59c5
Mugunthan93 <kskarthick93@gmail.com>
2020-03-05 08:24:41 +0530

    working in traveller pickr
    Signed-off-by: Mugunthan93 <kskarthick93@gmail.com>

--- src/app/pages/home/search/flight/one-way/one-way.page.ts
+++ src/app/pages/home/search/flight/one-way/one-way.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
-import { ModalController } from '@ionic/angular';
+import { ModalController, PickerController } from '@ionic/angular';
 import { CityModalComponent } from 'src/app/components/city-modal/city-modal.component';
 import { BookingService } from 'src/app/services/booking/booking.service';
 import { CalendarModalComponent } from 'src/app/components/calendar-modal/calendar-modal.component';
@@ -12,10 +12,46 @@ import { CalendarModalComponent } from 'src/app/components/calendar-modal/calend
 })
 export class OneWayPage implements OnInit {
 
-  oneWaySearch: FormGroup
+  oneWaySearch: FormGroup;
+  traveller = [
+    [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
+    ],
+    [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
+    ],
+    [
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9'
+    ]
+  ];
 
   constructor(
-    public modalCtrl : ModalController,
+    public modalCtrl: ModalController,
+    public pickrCtrl : PickerController,
     public fb : FormBuilder,
     public booking : BookingService
   ) {
@@ -69,4 +105,89 @@ export class OneWayPage implements OnInit {
     this.booking.search('flight');
   }
 
+  async selectPassenger(numColumns, numOptions) {
+    const pickr = await this.pickrCtrl.create({
+      columns: [
+      {
+        name: 'Adult',
+        options: [
+          { text: 'One', value: '1' },
+          { text: 'two', value: '2' },
+          { text: 'three', value: '3' },
+          { text: 'four', value: '4' },
+          { text: 'five', value: '5' },
+          { text: 'six', value: '6' },
+          { text: 'seven', value: '7' },
+          { text: 'eight', value: '8' },
+          { text: 'nine', value: '9' }
+        ]
+      },
+      {
+        name: 'child',
+        options: [
+          { text: 'One', value: '1' },
+          { text: 'two', value: '2' },
+          { text: 'three', value: '3' },
+          { text: 'four', value: '4' },
+          { text: 'five', value: '5' },
+          { text: 'six', value: '6' },
+          { text: 'seven', value: '7' },
+          { text: 'eight', value: '8' },
+          { text: 'nine', value: '9' }
+        ]
+        },
+        {
+          name: 'infant',
+          options: [
+            { text: 'One', value: '1' },
+            { text: 'two', value: '2' },
+            { text: 'three', value: '3' },
+            { text: 'four', value: '4' },
+            { text: 'five', value: '5' },
+            { text: 'six', value: '6' },
+            { text: 'seven', value: '7' },
+            { text: 'eight', value: '8' },
+            { text: 'nine', value: '9' }
+          ]
+        }
+      ],
+      buttons: [
+        {
+          text: 'cancel',
+          role : 'cancel'
+        },
+        {
+          text: 'confirm',
+          role: 'confirm',
+          handler: (val) => {
+            console.log(val);
+          }
+        }
+      ]
+    });
+
+    return await pickr.present();;
+  }
+
+  getColumns(numColumns, numOptions, columnOptions) {
+  let columns = [];
+  for (let i = 0; i < numColumns; i++) {
+    columns.push({
+      name: `col-${i}`,
+      options: this.getColumnOptions(i, numOptions, columnOptions)
+    });
+  }
+
+  return columns;
+}
+
+  getColumnOptions(columnIndex, numOptions, columnOptions) {
+    let options = [];
+    for (let i = 0; i < numOptions; i++) {
+      options.push({
+        text: columnOptions[columnIndex][i % numOptions],
+        value: i
+      })
+    }
+  }
 }
