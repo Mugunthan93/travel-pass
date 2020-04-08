58f2e4bce73375caa73655e33ffa344aefd04799
Mugunthan93 <kskarthick93@gmail.com>
2020-04-08 23:28:56 +0530

    passenger details ui completed
    Signed-off-by: Mugunthan93 <kskarthick93@gmail.com>

--- src/app/components/passenger-detail/passenger-detail.component.ts
+++ src/app/components/passenger-detail/passenger-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MealBaggageComponent } from '../meal-baggage/meal-baggage.component';
+import { CalendarModalOptions, CalendarModal } from 'ion2-calendar';
 
 @Component({
   selector: 'app-passenger-detail',
@@ -29,6 +30,32 @@ export class PassengerDetailComponent implements OnInit {
 
     return await modal.present();
   }
+
+  async getCalendar(title) {
+      const options: CalendarModalOptions = {
+        title: title,
+        pickMode: 'single',
+        color: 'dark',
+        weekStart: 1,
+        canBackwardsSelected: false,
+        closeLabel: 'Close',
+        doneLabel: 'OK',
+        defaultDate: Date.now()
+      }
+      const modal = await this.modalCtrl.create({
+        component: CalendarModal,
+        componentProps: {
+          options
+        }
+      });
+
+      modal.present();
+
+    const event: any = await modal.onDidDismiss();
+    console.log(event);
+  }
+
+
 
   dismissDetail() {
     this.modalCtrl.dismiss(null, null, 'passenger-details');
--- src/app/pages/home/book/book.module.ts
+++ src/app/pages/home/book/book.module.ts
@@ -10,13 +10,15 @@ import { BookPage } from './book.page';
 import { PassengerInfoComponent } from 'src/app/components/passenger-info/passenger-info.component';
 import { PassengerDetailComponent } from 'src/app/components/passenger-detail/passenger-detail.component';
 import { MealBaggageComponent } from 'src/app/components/meal-baggage/meal-baggage.component';
+import { CalendarModule } from 'ion2-calendar';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    BookPageRoutingModule
+    BookPageRoutingModule,
+    CalendarModule
   ],
   declarations: [
     BookPage,
