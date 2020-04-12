084bc854f6e5d160af4de8b4cd5e8f82d07ef099
Mugunthan93 <kskarthick93@gmail.com>
2020-04-12 11:24:33 +0530

    location for hotel filter is added
    Signed-off-by: Mugunthan93 <kskarthick93@gmail.com>

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -7,8 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
+  options = {
+    tourist: [
+      { value: "East Coast Road(ECR)", selection: false },
+      { value:"Kodambakkam", selection: false },
+      { value:"Guindy", selection: false },
+      { value: "Alwarpet", selection: false }
+    ],
+    bussiness_shopping: [
+      { value: "Anna Salai", selection: false },
+      { value: "T nagar", selection: false },
+      { value: "Anna Nagar", selection: false }
+    ],
+    transit: [
+      { value: "Internation Airport", selection: false },
+      { value: "Egmore Station", selection: false },
+      { value: "Central Station", selection: false }
+    ],
+    topareas: [
+      { value: "Thousand Lights", selection: false },
+      { value: "Triplicane", selection: false },
+      { value: "Periyamet", selection: false }
+    ]
+  }
+
   constructor() { }
 
   ngOnInit() {}
+
+  chipSelection(property: any, index: number) {
+    console.log(property);
+    property.selection = !property.selection;
+  }
 
 }
